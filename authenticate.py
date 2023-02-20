37a241dfb4900ada8a641f9eff6f33508a302b0a
Marina <111331059+MarinaFlynne@users.noreply.github.com>
2023-02-20 15:17:22 -0800

    add is_valid_credentials function

--- authenticate.py
+++ authenticate.py
@@ -2,4 +2,23 @@
 """
 TODO write desription of file
 """
+import yaml
 
+def main():
+    username = input("Username: ")
+    password = input("Password: ")
+    if is_valid_credentials(username, password):
+        print("Valid Credentials")
+    else:
+        print("Invalid Credentials")
+
+
+def is_valid_credentials(username: str, password: str) -> bool:
+    if username == "Marina" and password == "1234":
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    main()
