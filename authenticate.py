c85baa5d0a987d141c29e888240f669847c40a44
Marina <111331059+MarinaFlynne@users.noreply.github.com>
2023-02-20 21:09:19 -0800

    make authenticate.py check database

--- authenticate.py
+++ authenticate.py
@@ -4,8 +4,9 @@ Checks if the given credentials exist in the database
 """
 import yaml
 from hashlib import sha256
+import sqlite3
 
-CREDENTIAL_FILE = "credentials.yaml"
+CONFIG_FILENAME = "config.yaml"
 
 
 def main():
@@ -17,31 +18,41 @@ def main():
         print("Invalid Credentials")
 
 
+def get_database_filename() -> str:
+    # open config file and get dict of values
+    config_file = open(CONFIG_FILENAME)
+    config_dict = yaml.safe_load(config_file)[0]
+    config_file.close()
+
+    # get database filename from the config
+    database_filename = config_dict['database_filename']
+    return database_filename
+
+
 def hash_string(string: str) -> str:
     string = string.encode('utf-8')
     string = sha256(string).hexdigest()
     return string
 
 
-def get_credential_dict() -> dict:
-    file = open(CREDENTIAL_FILE, "r")
-    credential_list: list = yaml.safe_load(file)
-    file.close()
-
-    username_to_password_hash = {}
-    for credential in credential_list:
-        username_to_password_hash[credential["username"]] = credential["password_hash"]
-
-    return username_to_password_hash
-
-
 def is_valid_credentials(username: str, password: str) -> bool:
-    username_to_password_hash: dict = get_credential_dict()
+    database_filename = get_database_filename()
 
-    if username_to_password_hash[username] == hash_string(password):
-        return True
+    connection = sqlite3.connect(database_filename)
+    cursor = connection.cursor()
+
+    # get password
+    cursor.execute(f"SELECT password_hash FROM users WHERE username = '{username}'")
+    row = cursor.fetchone()
+
+    password_hash = ""
+    if row is not None:
+        password_hash = row[0]
     else:
         return False
+
+    if password_hash == hash_string(password):
+        return True
 
 
 if __name__ == "__main__":
