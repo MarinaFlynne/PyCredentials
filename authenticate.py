6d46157eff1676f8851b7784eca81168d539f350
Marina <111331059+MarinaFlynne@users.noreply.github.com>
2023-02-20 17:40:37 -0800

    add yaml reading functionality

--- authenticate.py
+++ authenticate.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 """
-TODO write desription of file
+TODO write description of file
 """
 import yaml
+from hashlib import sha256
+
+CREDENTIAL_FILE = "credentials.yaml"
+
 
 def main():
     username = input("Username: ")
@@ -12,9 +16,29 @@ def main():
     else:
         print("Invalid Credentials")
 
+
+def hash_string(string: str) -> str:
+    string = string.encode('utf-8')
+    string = sha256(string).hexdigest()
+    return string
+
+
+def get_credential_dict() -> dict:
+    file = open(CREDENTIAL_FILE, "r")
+    credential_list: list = yaml.safe_load(file)
+    file.close()
+
+    username_to_password_hash = {}
+    for credential in credential_list:
+        username_to_password_hash[credential["username"]] = credential["password_hash"]
+
+    return username_to_password_hash
+
+
 def is_valid_credentials(username: str, password: str) -> bool:
-    accounts = {"Marina": "1234", "Flynne": "password"}
-    if username == "Marina" and password == "1234":
+    username_to_password_hash: dict = get_credential_dict()
+
+    if username_to_password_hash[username] == hash_string(password):
         return True
     else:
         return False
